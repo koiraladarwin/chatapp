431e5fd52b5797b187df2755ad9d0ffe676e7fb8
Darwin Koirala <darwinkoirala123@gmail.com>
2025-07-06 19:12:41 +0545

    added lanfing page sucessfully

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,12 @@ import './App.css'
 import Login from './features/auth/pages/Login'
 import Signup from './features/auth/pages/Signup'
 import Chat from './features/chat/pages/Chat'
+import Landing from './features/landing/Landing'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" ></Route>
+      <Route path="/" element={<Landing/>}></Route>
       <Route path="/chat" element={<Chat />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
